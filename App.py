390cdc184489811612d6182124c32b11ae44ef3f
Ajay Surya <141923850+AjaySurya-018@users.noreply.github.com>
2024-11-21 03:01:40 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import pulp
 import os
 import seaborn as sns
-import scipy.stats as stats
 
 # Function to fetch distinct origin and destination airport codes
 def fetch_airport_codes():
